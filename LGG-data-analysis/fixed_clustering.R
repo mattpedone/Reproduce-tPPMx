e69a935d2998c000fb5cb7290bdbda1ed278915b
mattpedone <mattpedone7@gmail.com>
2023-02-09 09:56:36 +0100

    tidying

--- LGG-data-analysis/fixed_clustering.R
+++ LGG-data-analysis/fixed_clustering.R
@@ -20,7 +20,7 @@ trtsgn <- c(matchRTComp[,10]) + 1
 npat <- length(trtsgn)
 #trtsgn <- sample(1:2, npat, replace = TRUE)
 
-K <- 50#numero di fold
+K <- 1#numero di fold
 
 predAPT_all <- matrix(0, nrow = npat, ncol = 9)
 nclust_all <- matrix(0, nrow = K, ncol = 6)
@@ -42,37 +42,37 @@ nout <- 2000
 X <- scale(matchRTComp[,16:38])
 Z <- scale(matchRTComp[,c(11,13)])
 
-#myres0 <- foreach(k = 1:K) %dorng%
-#  {
-#    modelpriors <- list()
-#    modelpriors$hP0_m0 <- rep(0, ncol(Y)); modelpriors$hP0_nu0 <- 10
-#    modelpriors$hP0_s0 <- ncol(Y) + 2; modelpriors$hP0_Lambda0 <- 10
-#
-#    #n_aux <- 5 # auxiliary variable for Neal's Algorithm 8
-#    vec_par <- c(0.0, 1.0, .5, 1.0, 2.0, 2.0, 0.1)
-#    #double m0=0.0, s20=10.0, v=.5, k0=1.0, nu0=2.0, n0 = 2.0;
-#    iterations <- 12000
-#    burnin <- 2000
-#    thinning <- 5
-#
-#    nout <- (iterations-burnin)/thinning
-#    predAPT <- c()
-#
-#    res0 <- tryCatch(expr = ppmxct(y = data.matrix(Y), X = data.frame(X),
-#                                   Xpred = data.frame(X[1:2,]), Z = data.frame(Z),
-#                                   Zpred = data.frame(Z[1:2,]), asstreat = trtsgn, #treatment,
-#                                   PPMx = 1, cohesion = 2, kappa = c(1, 20, 10, 1), sigma = c(0.01, .59, 6),
-#                                   similarity = 2, consim = 2, similparam = vec_par,
-#                                   calibration = 2, coardegree = 2, modelpriors,
-#                                   update_hierarchy = F,
-#                                   hsp = T, iter = iterations, burn = burnin, thin = thinning,
-#                                   mhtunepar = c(0.05, 0.05), CC = 5, reuse = 1,
-#                                   nclu_init = 3), error = function(e){FALSE})
-#    return(res0)
-#  }
+myres0 <- foreach(k = 1:K) %dorng%
+  {
+    modelpriors <- list()
+    modelpriors$hP0_m0 <- rep(0, ncol(Y)); modelpriors$hP0_nu0 <- 10
+    modelpriors$hP0_s0 <- ncol(Y) + 2; modelpriors$hP0_Lambda0 <- 10
 
-#save(myres0, file = "output/lgg-da/lgg_rep4clu.RData")
-load("output/lgg-da/lgg_rep4clu.RData")
+    #n_aux <- 5 # auxiliary variable for Neal's Algorithm 8
+    vec_par <- c(0.0, 1.0, .5, 1.0, 2.0, 2.0, 0.1)
+    #double m0=0.0, s20=10.0, v=.5, k0=1.0, nu0=2.0, n0 = 2.0;
+    iterations <- 12000
+    burnin <- 2000
+    thinning <- 5
+
+    nout <- (iterations-burnin)/thinning
+    predAPT <- c()
+
+    res0 <- tryCatch(expr = ppmxct(y = data.matrix(Y), X = data.frame(X),
+                                   Xpred = data.frame(X[1:2,]), Z = data.frame(Z),
+                                   Zpred = data.frame(Z[1:2,]), asstreat = trtsgn, #treatment,
+                                   PPMx = 1, cohesion = 2, kappa = c(1, 20, 10, 1), sigma = c(0.01, .59, 6),
+                                   similarity = 2, consim = 2, similparam = vec_par,
+                                   calibration = 2, coardegree = 2, modelpriors,
+                                   update_hierarchy = T,
+                                   hsp = T, iter = iterations, burn = burnin, thin = thinning,
+                                   mhtunepar = c(0.05, 0.05), CC = 5, reuse = 1,
+                                   nclu_init = 3), error = function(e){FALSE})
+    return(res0)
+  }
+
+save(myres0, file = "output/lgg-da/lgg_rep4clunks.RData")
+#load("output/lgg-da/lgg_rep4clunks.RData")
 
 ccm <- matrix(0, nrow=79, ncol = 79)
 for(k in 1:K){
@@ -164,7 +164,7 @@ vartab <- rbind(apply(pred_cov_g1, 2, mean), apply(pred_cov_g2, 2, mean),
 df <- data.frame(cbind(Group = c(1:10), vartab))
 df$Group <- as.factor(df$Group)
 df <- reshape2::melt(df, id.vars = "Group")
-df <- df[which(df$Group != 3),]
+df <- df[which(df$Group != 2),]
 df <- df[which(df$Group != 5),]
 df <- df[which(df$Group != 6),]
 df <- df[which(df$Group != 7),]
@@ -179,7 +179,7 @@ p <- ggplot(df, aes(x=variable, y=value, group=Group)) +
   theme(axis.text.x=element_text(angle=45, hjust=1)) + ggtitle("Treatment 2")
 p
 table(matchRTComp[which(labels1 == 1),9])#higher the better response
-table(matchRTComp[which(labels1 == 2),9])#higher the better response
+table(matchRTComp[which(labels1 == 3),9])#higher the better response
 table(matchRTComp[which(labels1 == 4),9])#higher the better response
 table(matchRTComp[which(labels1 == 9),9])#higher the better response
 
@@ -207,6 +207,7 @@ g6 <- orp2[which(labels2==6)]
 g7 <- orp2[which(labels2==7)]
 g8 <- orp2[which(labels2==8)]
 g9 <- orp2[which(labels2==9)]
+g10 <- orp2[which(labels2==10)]
 
 reord <- c()
 for(i in 1:max(labels2)){
@@ -224,7 +225,7 @@ c1 <- ggplot(mC, aes(Var1,Var2, fill=`Co-occurrence`)) + geom_raster() +
   scale_fill_continuous(type = "viridis") +
   theme_classic() +
   theme(legend.position="bottom") +
-  xlab("Patients") + ylab("Patients") + ggtitle("Treatment 1")#+ ggtitle("Heatmap of averaged co-occurence matrix")
+  xlab("Patients") + ylab("Patients") + ggtitle("Treatment 2")#+ ggtitle("Heatmap of averaged co-occurence matrix")
 c1
 #ggsave(c1, device = "pdf", path = "figs", filename = "avg_coocc.pdf")
 
@@ -239,14 +240,15 @@ pred_cov_g6 <- pred_cov[as.character(g6),]
 pred_cov_g7 <- pred_cov[as.character(g7),]
 pred_cov_g8 <- pred_cov[as.character(g8),]
 pred_cov_g9 <- pred_cov[as.character(g9),]
+pred_cov_g10 <- pred_cov[as.character(g10),]
 
 vartab <- rbind(apply(pred_cov_g1, 2, var), apply(pred_cov_g2, 2, var),
                 apply(pred_cov_g3, 2, var), apply(pred_cov_g4, 2, var),
                 apply(pred_cov_g5, 2, var), apply(pred_cov_g6, 2, var),
                 apply(pred_cov_g7, 2, var), apply(pred_cov_g8,  2, var),
-                apply(pred_cov_g9, 2, var))
+                apply(pred_cov_g9, 2, var), apply(pred_cov_g10, 2, var))
 #vartab <- rbind(apply(pred_cov_g1, 2, mean), apply(pred_cov_g2, 2, mean), apply(pred_cov_g3, 2, mean), apply(pred_cov_g4, 2, mean), apply(pred_cov_g5, 2, mean))
-df <- data.frame(cbind(Group = c(1:9), vartab))
+df <- data.frame(cbind(Group = c(1:10), vartab))
 df$Group <- as.factor(df$Group)
 #df <- df[which(df$Group != 9),]
 df <- reshape2::melt(df, id.vars = "Group")
@@ -264,15 +266,18 @@ vartab <- rbind(apply(pred_cov_g1, 2, mean), apply(pred_cov_g2, 2, mean),
                 apply(pred_cov_g3, 2, mean), apply(pred_cov_g4, 2, mean),
                 apply(pred_cov_g5, 2, mean), apply(pred_cov_g6, 2, mean),
                 apply(pred_cov_g7, 2, mean), apply(pred_cov_g8, 2, mean),
-                apply(pred_cov_g9, 2, mean))
-df <- data.frame(cbind(Group = c(1:9), vartab))
+                apply(pred_cov_g9, 2, mean), apply(pred_cov_g10, 2, mean))
+df <- data.frame(cbind(Group = c(1:10), vartab))
 df$Group <- as.factor(df$Group)
 df <- reshape2::melt(df, id.vars = "Group")
-df <- df[which(df$Group != 4),]
+df <- df[which(df$Group != 3),]
 df <- df[which(df$Group != 5),]
 df <- df[which(df$Group != 6),]
+df <- df[which(df$Group != 7),]
 df <- df[which(df$Group != 8),]
 df <- df[which(df$Group != 9),]
+df <- df[which(df$Group != 10),]
+
 p <- ggplot(df, aes(x=variable, y=value, group=Group)) +
   geom_line(aes(color=Group))+
   geom_point(aes(color=Group)) +
@@ -285,7 +290,8 @@ p
 table(matchRTComp[which(labels2 == 1),9])#higher the better response
 table(matchRTComp[which(labels2 == 2),9])#higher the better response
 table(matchRTComp[which(labels2 == 3),9])#higher the better response
-table(matchRTComp[which(labels2 == 7),9])#higher the better response
+table(matchRTComp[which(labels2 == 4),9])#higher the better response
+table(matchRTComp[which(labels2 == 9),9])#higher the better response
 
 fix_clu <- cbind(labels1, labels2)
 #table(labels1)
@@ -296,22 +302,22 @@ burnin <- 2000
 thinning <- 5
 SP <- (iterations-burnin)/thinning
 modelpriors <- list()
-modelpriors$hP0_m0 <- rep(0, ncol(Y)); modelpriors$hP0_nu0 <- .10
-modelpriors$hP0_s0 <- ncol(Y) + 2; modelpriors$hP0_Lambda0 <- .10
+modelpriors$hP0_m0 <- rep(0, ncol(Y)); modelpriors$hP0_nu0 <- 10
+modelpriors$hP0_s0 <- ncol(Y) + 2; modelpriors$hP0_Lambda0 <- 10
 
 vec_par <- c(0.0, 1.0, .5, 1.0, 2.0, 2.0, 0.1)
 
 
-#fclu <- ppmxct_fixed(y = data.matrix(Y), X = data.frame(X),
-#                     Z = data.frame(Z), asstreat = trtsgn, #treatment,
-#                     PPMx = 1, cohesion = 1, kappa = c(1, 20, 10, 1), sigma = c(0.01, .59, 6),
-#                     similarity = 2, consim = 2, similparam = vec_par, calibration = 2,
-#                     coardegree = 2, modelpriors, update_hierarchy = F, hsp = F,
-#                     iter = iterations, burn = burnin, thin = thinning,
-#                     mhtunepar = c(0.05, 0.05), CC = 5, reuse = 1, fix_clu = fix_clu)
+fclu <- ppmxct_fixed(y = data.matrix(Y), X = data.frame(X),
+                     Z = data.frame(Z), asstreat = trtsgn, #treatment,
+                     PPMx = 1, cohesion = 1, kappa = c(1, 20, 10, 1), sigma = c(0.01, .59, 6),
+                     similarity = 2, consim = 2, similparam = vec_par, calibration = 2,
+                     coardegree = 2, modelpriors, update_hierarchy = F, hsp = T,
+                     iter = iterations, burn = burnin, thin = thinning,
+                     mhtunepar = c(0.05, 0.05), CC = 5, reuse = 1, fix_clu = fix_clu)
 
-#save(fclu, file = "output/lgg-da/lggfixedclu.RData")
-load("output/lgg-da/lggfixedclu.RData")
+save(fclu, file = "output/lgg-da/lggfixedclunks.RData")
+load("output/lgg-da/lggfixedclunks.RData")
 
 pi1 <- fclu$pi_out[which(trtsgn == 1),,]
 pi11 <- pi1[which(labels1 == 1),,]
@@ -320,33 +326,31 @@ pi11 <- t(apply(pi11, c(2, 3), mean))
 
 colnames(pi11) <- c("PD", "PS", "CR")
 p11 <- ggtern(data=data.frame(pi11), aes(x=PD, y=PS, z=CR)) + #geom_point()
-  geom_density_tern(colour = "#F8766D", bdl = 0.00000001) +
+  geom_density_tern(colour = "#F8766D", bdl = 0.1) +
   ggtitle("Group 1")
 
-#pi12 <- pi1[which(labels1 == 2),,]
-#
-#pi12 <- t(apply(pi12, c(2, 3), mean))
-#pi12 <- pi12[-which(pi12[,1]<.01),]
-#pi12 <- pi12[-which(pi12[,2]>.8),]
-#pi12 <- pi12[-which(pi12[,3]>.31),]
-#pi12 <- pi12[-which(pi12[,3]<.1),]
-#pi12 <- pi12[sample(1:(dim(pi12)[1]), 1000, T),]
-#
-#colnames(pi12) <- c("PD", "PS", "CR")
-#p12 <- ggtern(data=data.frame(pi12), aes(x=PD, y=PS, z=CR)) +
-#  geom_density_tern(colour = "#F8766D", bdl = 0.000001) +
-#  ggtitle("Group 2")
-#p12
+pi13 <- pi1[which(labels1 == 3),,]
 
-#pi14 <- pi1[which(labels1 == 4),,]
-#
-#pi14 <- as.data.frame(t(apply(pi14, c(2, 3), mean)))
+pi13 <- t(apply(pi13, c(2, 3), mean))
+#pi12 <- abs(pi12 + rnorm((dim(pi12)[1]), 0, .01))
+#pi12 <- pi12/apply(pi12, 1, sum)
+
+colnames(pi13) <- c("PD", "PS", "CR")
+p13 <- ggtern(data=data.frame(pi13), aes(x=PD, y=PS, z=CR)) + #geom_point() +
+  geom_density_tern(colour = "#F8766D", bdl = 0.1) +
+  ggtitle("Group 3")
+p13
+
+pi14 <- pi1[which(labels1 == 4),,]
+#pi14 <- abs(pi14 + rnorm((dim(pi14)[1]), 0, .05))
+
+pi14 <- as.data.frame(t(apply(pi14, c(2, 3), mean)))
 #pi14 <- pi14[sample(1:2000, 100),]
-#colnames(pi14) <- c("PD", "PS", "CR")
-#p14 <- ggtern(data=pi14, aes(x=PD, y=PS, z=CR)) +
-#  geom_density_tern(colour = "#F8766D", bdl = 0.00000001) +
-#  ggtitle("Group 4")
-#p14
+colnames(pi14) <- c("PD", "PS", "CR")
+p14 <- ggtern(data=pi14, aes(x=PD, y=PS, z=CR)) + #geom_point() +
+  geom_density_tern(colour = "#F8766D", bdl = 0.1) +
+  ggtitle("Group 4")
+
 
 pi19 <- pi1[which(labels1 == 9),,]
 
@@ -354,64 +358,55 @@ pi19 <- t(apply(pi19, c(2, 3), mean))
 
 colnames(pi19) <- c("PD", "PS", "CR")
 p19 <- ggtern(data=data.frame(pi19), aes(x=PD, y=PS, z=CR)) +
-  geom_density_tern(colour = "#F8766D", bdl = 0.00000001) +
+  geom_density_tern(colour = "#F8766D", bdl = 0.1) +
   ggtitle("Group 9")
 
-tern1 <- ggtern::grid.arrange(p11, p19, ncol = 2)
+tern1 <- ggtern::grid.arrange(p11, p13, p14, p19, ncol = 2)
 
 pi2 <- fclu$pi_out[which(trtsgn == 2),,]
 pi21 <- pi2[which(labels2 == 1),,]
 
-pi21 <- t(apply(pi21, c(2, 3), mean))
+pi21 <- t(apply(pi21, c(2, 3), mean))+.01
+#pi21 <- pi21+abs(rnorm(dim(pi21)[1], 0, 0.01))
+pi21 <- pi21/apply(pi21,1, sum)
 
 colnames(pi21) <- c("PD", "PS", "CR")
 p21 <- ggtern(data=data.frame(pi21), aes(x=PD, y=PS, z=CR)) + #geom_point()
-  geom_density_tern(colour = "#F8766D", bdl = 0.00000001) +
+  geom_density_tern(colour = "#F8766D", bdl = 0.1) +
   ggtitle("Group 1")
 p21
 
 pi22 <- pi2[which(labels2 == 2),,]
 
 pi22 <- t(apply(pi22, c(2, 3), mean))
-#pi12 <- pi12[-which(pi12[,1]<.01),]
-#pi12 <- pi12[-which(pi12[,2]>.8),]
-#pi12 <- pi12[-which(pi12[,3]>.31),]
-#pi12 <- pi12[-which(pi12[,3]<.1),]
-#pi12 <- pi12[sample(1:(dim(pi12)[1]), 1000, T),]
 
 colnames(pi22) <- c("PD", "PS", "CR")
 p22 <- ggtern(data=data.frame(pi22), aes(x=PD, y=PS, z=CR)) +
-  geom_density_tern(colour = "#F8766D", bdl = 0.000001) +
+  geom_density_tern(colour = "#F8766D", bdl = 0.1) +
   ggtitle("Group 2")
-p22
 
 pi23 <- pi2[which(labels2 == 3),,]
 
 pi23 <- as.data.frame(t(apply(pi23, c(2, 3), mean)))
-pi23 <- pi23[-which(pi23[,1]<.05),]
-#pi23 <- pi23[-which(pi23[,3]<.1),]
-
-#pi23[,1] <- pi23[,1] + abs(rnorm(dim(pi23)[1], 0, .01))
-#pi23 <- pi23/apply(pi23, 1, sum)
-summary(pi23)
 colnames(pi23) <- c("PD", "PS", "CR")
 p23 <- ggtern(data=pi23, aes(x=PD, y=PS, z=CR)) +
-  geom_density_tern(colour = "#F8766D", bdl = 0.00000001) +
+  geom_density_tern(colour = "#F8766D", bdl = 0.1) +
   ggtitle("Group 3")
-p23
 
-#pi27 <- pi2[which(labels1 == 7),,]
-#
-#pi27 <- t(apply(pi27, c(2, 3), mean))
-#pi27 <- pi27[-which(pi27[,1]<.1),]
-#pi27 <- pi27[-which(pi27[,3]<.1),]
-#
-#colnames(pi27) <- c("PD", "PS", "CR")
-#p27 <- ggtern(data=data.frame(pi27), aes(x=PD, y=PS, z=CR)) +
-#  geom_density_tern(colour = "#F8766D", bdl = 0.00000001) +
-#  ggtitle("Group 7")
-#p27
+pi24 <- pi2[which(labels2 == 4),,]
 
+pi24 <- as.data.frame(t(apply(pi24, c(2, 3), mean)))
+colnames(pi24) <- c("PD", "PS", "CR")
+p24 <- ggtern(data=pi24, aes(x=PD, y=PS, z=CR)) +
+  geom_density_tern(colour = "#F8766D", bdl = 0.1) +
+  ggtitle("Group 4")
 
-tern2 <- ggtern::grid.arrange(p21, p22, p23, ncol = 3)
+pi29 <- pi2[which(labels2 == 9),,]
+pi29 <- as.data.frame(t(apply(pi29, c(2, 3), mean)))
+colnames(pi29) <- c("PD", "PS", "CR")
+p29 <- ggtern(data=pi29, aes(x=PD, y=PS, z=CR)) +
+  geom_density_tern(colour = "#F8766D", bdl = 0.1) +
+  ggtitle("Group 9")
+
+tern2 <- ggtern::grid.arrange(p21, p22, p23, p24, p29, ncol = 3)
 
---
+++ LGG-data-analysis/lgg_samplerJF.R
@@ -0,0 +1,188 @@
+rm(list=ls())
+set.seed(121)
+
+library(rstan)
+library(parallel)
+library(doParallel)
+library(doRNG)
+
+#options(mc.cores = parallel::detectCores())
+registerDoParallel(cores = 10)
+rstan_options(auto_write = TRUE)
+
+npc2 <- function(output, trtsgn, myoutot){
+  K <- dim(output)[3]
+  n <- dim(output)[1]
+  myctut <- array(0, dim = c(3, 3, K))
+  myctutSum <- NULL
+  for (i in 1:K) {
+    mycurdata <- output[, , i]
+    mypre <- NULL
+    pretrt1 <- apply(mycurdata[, 1:3], 1, which.max)
+    pretrt2 <- apply(mycurdata[, 4:6], 1, which.max)
+    mypreTall <- cbind(pretrt1, pretrt2)
+    for (j in 1:n) {
+      mypre[j] <- mypreTall[j, trtsgn[j]]
+    }
+    sts <- table(mypre, myoutot)
+    mysdls <- as.numeric(rownames(sts))
+    str1 <- matrix(0, nrow = 3, ncol = 3)
+    str1[mysdls, ] <- sts
+    myctut[, , i] <- str1 * diag(3)
+    myctutSum[i] <- sum(str1 * diag(3))
+  }
+  res <- cbind(myctutSum)
+  return(res)
+}
+
+load("data/LGGdata.rda")
+matchRTComp <- matchRTComp[sample(1:nrow(matchRTComp), size = nrow(matchRTComp), replace = F),]
+trtsgn <- c(matchRTComp[,10]) + 1
+#trtsgn <- ifelse(trtsgn == 1, 2, 1)
+npat <- length(trtsgn)
+#trtsgn <- sample(1:2, npat, replace = TRUE)
+
+K <- 10#numero di fold
+
+predAPT_all <- matrix(0, nrow = npat, ncol = 9)
+nclust_all <- matrix(0, nrow = K, ncol = 6)
+gof_all <- matrix(0, nrow = K, ncol = 2)
+
+wk <- c(0, 40, 100)
+
+registerDoParallel(cores = 10)#alloco solo core necessari
+
+Y <- matrix(0, nrow = npat, ncol = max(as.numeric(matchRTComp[,9])))
+for(i in 1:nrow(Y)){
+  Y[i, as.numeric(matchRTComp[i,9])] <- 1
+}
+
+table(matchRTComp[,9:10])
+vectf <- c(1, 17, 33, 49, 65, 81, 97, 113, 129, 145, 159)
+#load("/home/matt/Dropbox/PHD/study-treatppmx/output/lgg12aprs121.RData")
+nout <- 2000
+X <- scale(matchRTComp[,16:38])
+Z <- scale(matchRTComp[,c(11,13)])
+
+#Yl <- Y
+#Xl <- X
+#Zl <- Z
+#trtl <- trtsgn
+
+R <- length(Yl)
+n <- dim(Yl[[1]])[1]
+J <- dim(Yl[[1]])[2]
+p <- dim(Xl[[1]])[2]
+q <- dim(Zl[[1]])[2]
+
+K <- 50#repliche
+npat_pred <- 28
+
+Y <- array(0, dim = c(n + npat_pred, J, R))
+X <- array(0, dim = c(n + npat_pred, p, R))
+Z <- array(0, dim = c(n + npat_pred, q, R))
+Xdis <- array(0, dim = c(n + npat_pred, (p + p + q + 1), R))
+trt <- matrix(0, n + npat_pred, R)
+
+
+for(r in 1:R){
+  Y[1:152,,r] <- Yl[[r]]
+  Y[153:180,,r] <- Y[125:152,,r]
+  X[1:152,,r] <- Xl[[r]]
+  X[153:180,,r] <- X[125:152,,r]
+  Z[1:152,,r] <- Zl[[r]]
+  Z[153:180,,r] <- Z[125:152,,r]
+  trt[1:124,r] <- trtl[[r]][1:124]-1
+  trt[125:152,r] <- 0
+  trt[153:180,r] <- 1
+  Xdis[,,r] <- cbind(X[,,r], Z[,,r], trt[,r], (trt[,r]*X[,,r]))
+}
+
+#params1 <- params2 <- array(npat_pred, J, K)
+
+predAPT_all <- array(0, dim = c(npat_pred, 9, K))
+nclust_all <- matrix(0, nrow = K, ncol = 6)
+gof_all <- matrix(0, nrow = K, ncol = 2)
+
+wk <- c(0, 40, 100)
+
+res <- foreach(k = 1:K) %dorng%
+  {
+    X_train <- Xdis[1:124, ,k]
+    X_train <- X_train[which(trt[1:124,k] == 0),]
+    Y_train <- Y[1:124, ,k]
+    Y_train <- Y_train[which(trt[1:124,k] == 0),]
+
+    X_test <- Xdis[125:180,,k]
+    Y_test <- Y[125:180,,k]
+
+    ss_data = list(N = dim(X_train)[1], M = dim(X_test)[1], J = dim(Y_train)[2],
+                    P = dim(X_train)[2], X = X_train, Xp = X_test, Y = Y_train,
+                    sd_prior = 1.0, psi = .25)
+
+    fit <- rstan::stan(file = "model.stan", data = ss_data, cores = 1, iter = 100,
+                        chains = 1, verbose = T, warmup = 50, seed = 121,
+                        control = list(max_treedepth = 15, adapt_delta = 0.995))#995))
+
+
+    #check_hmc_diagnostics(fit1)
+    params <- rstan::extract(fit)
+    pp <- apply(params$pipred, c(2,3), median)
+
+    out <- cbind(pp1 = pp[1:28,], pp2 = pp[29:56,])
+    return(out)
+  }
+
+#summ <- rstan::summary(fit)$summary
+
+for(k in 1:K){
+  pp1 <- res[[k]][,1:3]
+  A1 <- pp1%*%wk
+  pp2 <- res[[k]][,4:6]
+  A2 <- pp2%*%wk
+
+  predAPT_all[, 1, k] <- A1
+  predAPT_all[, 2, k] <- A2
+  myt <- as.numeric(A1 < A2) + 1
+  predAPT_all[, 3, k] <- myt
+  predAPT_all[, 4:9, k] <- cbind(pp1, pp2)
+
+  myprob <- simdata$prob[[k]]
+}
+mywk1 <- myprob[[1]][125:152,]%*%wk
+mywk2 <- myprob[[2]][125:152,]%*%wk
+optrt <- as.numeric(mywk1 < mywk2) + 1
+utsum <- sum(abs(mywk2 - mywk1))
+utdiff <- abs(as.numeric(mywk2 - mywk1))
+
+#MOT
+PPMXCT <- c()
+for(k in 1:K){
+  PPMXCT[k] <-  sum(abs(predAPT_all[, 3, k] - optrt))
+}
+
+MOT <- c(round(mean(PPMXCT), 4), round(sd(PPMXCT), 4))
+
+#MTUg
+PPMXpp <- c()
+for(k in 1:K){
+  PPMXpp[k] <- -(2*sum(abs((predAPT_all[, 3, k] - optrt)) * utdiff) - utsum);
+}
+
+MTUg <- c(round(mean(PPMXpp/utsum), 4), round(sd(PPMXpp/utsum), 4))
+
+#NPC
+PPMXCUT <- c()
+for(k in 1:K){
+  trtsgn_test <- simdata$trtsgn[[k]][125:152]
+  temp <- array(0, dim = c(28, 6, 1))
+  temp[,,1] <- predAPT_all[, 4:9,k]
+  myoutot <- simdata$yord[[k]][125:152,]
+  PPMXCUT[k] <- npc2(temp, trtsgn_test, myoutot)
+}
+NPC <- c(round(mean(PPMXCUT), 4), round(sd(PPMXCUT), 4))
+
+#results
+resDMHS <- rbind(MOT, MTUg, NPC)#, WAIC, lpml)
+colnames(resDMHS) <- c("mean", "sd")
+save(resDMHS, file="output/simulation-study/main/scen1a_dmhs_res.RData")
