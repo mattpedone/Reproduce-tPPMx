2d4432390dfb46442c5ebf355ce15a73903ff0d2
mattpedone <mattpedone7@gmail.com>
2023-02-17 10:06:09 +0100

    linear scenarios

--- R/genscen.R
+++ R/genscen.R
@@ -29,3 +29,28 @@ save(scen3a, file = "data/scen3a.RData")
 # Scenario 3b
 scen3b <- treatppmx::genmech_het(npred = 50, nset = 50, overlap = .8)
 save(scen3b, file = "data/scen3b.RData")
+
+# Simulation Study - supplementary (linear)
+# Scenario 1aL
+scen1aL <- treatppmx::genmech_hetL(npred = 25, nset = 50, overlap = 1.0)
+save(scen1aL, file = "data/scen1aL.RData")
+
+# Scenario 1bL
+scen1bL <- treatppmx::genmech_hetL(npred = 50, nset = 50, overlap = 1.0)
+save(scen1bL, file = "data/scen1bL.RData")
+
+# Scenario 2aL
+scen2aL <- treatppmx::genmech_hetL(npred = 25, nset = 50, overlap = .9)
+save(scen2aL, file = "data/scen2aL.RData")
+
+# Scenario 2bL
+scen2bL <- treatppmx::genmech_hetL(npred = 50, nset = 50, overlap = .9)
+save(scen2bL, file = "data/scen2bL.RData")
+
+# Scenario 3aL
+scen3aL <- treatppmx::genmech_hetL(npred = 25, nset = 50, overlap = .8)
+save(scen3aL, file = "data/scen3aL.RData")
+
+# Scenario 3bL
+scen3bL <- treatppmx::genmech_hetL(npred = 50, nset = 50, overlap = .8)
+save(scen3bL, file = "data/scen3bL.RData")
